d6e44303e72488ebe00873aee16ec5ae42ccfdbb
ihsanbara94 <ihsanbatubara94@gmail.com>
2023-01-23 16:19:05 +0700

    1d2cd1f

---
+++ _nuxt/static/1674465537/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fassesment\u002F_nuxt\u002Fstatic\u002F1674465537",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fassesment\u002F",assetsPath:"\u002Fassesment\u002F_nuxt\u002F",cdnURL:a}}}}(null));
